68d32304ecf251554ab15695bf4ec12b9d94bf40
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-04-05 00:30:59 -0500

    create Navbar with modal search

---
+++ src/components/CityName.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CityName() {
+  return (
+    <div>
+      <span>Locacion</span>
+      <span>Helsinki, Finland</span>
+    </div>
+  )
+}
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,35 +1,21 @@
-import { useEffect, useState } from "react";
-import Card from "../components/Card";
+import CityName from "../components/CityName";
 
 export default function SearchPage() {
-  const [location, setLocation] = useState([]);
-
-  useEffect(() => {
-    fetch('stays.json')
-      .then(res => res.json())
-      .then(data => {
-        setLocation(data);
-        console.log(data);
-      })
-  }, []);
-
-
   return (
-    <div className="px-14">
-      <div className="flex justify-between">
-        <h2 className="text-2xl font-bold">Stays in Finland</h2>
-        <span>12+ stays</span>
+    <div className="py-20 px-7 grid grid-cols-3 divide-x">
+      <button className="rounded-l-2xl focus:border-gray-600 focus:rounded-2xl hover:border border shadow flex flex-col text-left text-xs px-6 py-3 font-normal focus:font-bold"><p className="text-small-xs">LOCATION</p>Helsinki, Finland</button>
+      <button className="focus:border-gray-600 focus:rounded-2xl border shadow flex flex-col text-left text-xs px-4 py-3 font-normal focus:font-bold"><p className="text-small-xs">GUESTS</p>4 guests</button>
+      <div className="rounded-r-2xl shadow flex justify-center items-center ">
+        <button className="bg-orange-500 text-white px-6 py-3 rounded-2xl text-sm font-normal flex items-center">
+          <span className="material-icons text-white pr-2">
+            search
+          </span>
+          Search
+        </button>
       </div>
-      <div className="grid grid-cols-3 gap-2">
-        {
-          location.map((data) => {
-            return <div className="place-self-stretch">
-              <Card key={data.photo} children={data} />
-            </div>
-          })
-        }
+      <div>
+        <CityName />
       </div>
     </div>
-
   )
 }
