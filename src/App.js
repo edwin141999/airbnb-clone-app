7127b78f1228ac2e4a920e449c9e0ff7e69f7b1b
edwin141999 <183433@ids.upchiapas.edu.mx>
2022-04-06 01:00:18 -0500

    update Navbar and change App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import Navbar from './pages/Navbar';
 import HomePage from './pages/HomePage';
-import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className='px-16'>
-      <SearchPage/>
       <header>
         <Navbar />
       </header>
